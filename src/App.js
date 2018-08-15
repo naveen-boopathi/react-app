592327cc8ad3fb7763d9faf9aacacbe28c09930b
Naveen Boopathi <Naveenboopathi18@outlook.com>
2018-08-15 10:55:14 +0530

    Login Component created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Login from './Login.js';
-import NavBar from './Nav.js';
+import BookRide from './BookRide.js';
+import RideDetails from './RideDetails.js';
+import OfferRide from './OfferRide.js';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        <Login />
-      </div>
+      <Router>
+        <div>
+          <nav className="navbar navbar-default">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <button
+                  type="button"
+                  className="navbar-toggle collapsed"
+                  data-toggle="collapse"
+                  data-target="#bs-navbar-collapse-1"
+                  aria-expanded="false"
+                >
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar" />
+                  <span className="icon-bar" />
+                  <span className="icon-bar" />
+                </button>
+                <a className="navbar-brand" href="#">
+                  Share Cars
+                </a>
+              </div>
+              <div
+                className="collapse navbar-collapse"
+                id="bs-navbar-collapse-1"
+              >
+                <ul className="nav navbar-nav">
+                  <li>
+                    <Link to="/bookRide">Book Ride</Link>
+                  </li>
+                  <li>
+                    <Link to="/rideDetails">Ride Details</Link>
+                  </li>
+                  <li>
+                    <Link to="/offerRide">Offer Ride</Link>
+                  </li>
+                </ul>
+                <ul className="nav navbar-nav navbar-right">
+                  <li>
+                    <Link to="/">Login</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <Route exact path="/" component={Login} />
+          <Route path="/bookRide" component={BookRide} />
+          <Route path="/rideDetails" component={RideDetails} />
+          <Route path="/offerRide" component={OfferRide} />
+        </div>
+      </Router>
     );
   }
 }
--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,61 @@
 import React from 'react';
+import './Login.css';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password: '' };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value });
+  }
+  handleSubmit(e) {
+    alert('Form Submitted successfully!');
+    this.props.history.push('/bookRide');
+  }
   render() {
-    return <div>Login Component</div>;
+    return (
+      <div>
+        <div className="panel panel-primary">
+          <div className="panel-heading">
+            <h3 className="panel-title">Login</h3>
+          </div>
+          <div className="panel-body">
+            <form className="form" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  className="form-control"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
